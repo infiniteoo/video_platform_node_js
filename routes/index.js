711239c2a6447ac8dfecb48957aa0c1809821a50
Troy Dorman <troydorman@gmail.com>
2021-01-26 13:21:56 -0700

    eslint is set up along with the basic video player & route

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,49 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express')
+const router = express.Router()
+const fs = require('fs')
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+router.get('/', function (req, res, next) {
+  res.render('index', { title: 'Express' })
+})
 
-module.exports = router;
+router.get('/video', function (req, res) {
+  const path = 'assets/sample.mp4'
+  const stat = fs.statSync(path)
+  const fileSize = stat.size
+  const range = req.headers.range
+
+  if (range) {
+    const parts = range.replace(/bytes=/, '').split('-')
+    const start = parseInt(parts[0], 10)
+    const end = parts[1] ? parseInt(parts[1], 10) : fileSize - 1
+
+    if (start >= fileSize) {
+      res
+        .status(416)
+        .send('Requested range not satisfiable\n' + start + ' >= ' + fileSize)
+      return
+    }
+
+    const chunksize = end - start + 1
+    const file = fs.createReadStream(path, { start, end })
+    const head = {
+      'Content-Range': `bytes ${start}-${end}/${fileSize}`,
+      'Accept-Ranges': 'bytes',
+      'Content-Length': chunksize,
+      'Content-Type': 'video/mp4'
+    }
+
+    res.writeHead(206, head)
+    file.pipe(res)
+  } else {
+    const head = {
+      'Content-Length': fileSize,
+      'Content-Type': 'video/mp4'
+    }
+    res.writeHead(200, head)
+    fs.createReadStream(path).pipe(res)
+  }
+})
+
+module.exports = router
