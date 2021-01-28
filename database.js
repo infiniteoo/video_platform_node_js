c10fca4251cfb40661c8b84d7028213c60b84ca6
Troy Dorman <troydorman@gmail.com>
2021-01-27 20:33:21 -0700

    added posters to database and deleted a bunch of useless pics

--- database.js
+++ database.js
@@ -1,36 +1,42 @@
 const database = [
   {
-    path: 'video_library/Friday ( 1995 ) 720p BrRip x264 650MB YIFY.mp4',
+    video_path: 'video_library/Friday ( 1995 ) 720p BrRip x264 650MB YIFY.mp4',
+    poster_path: 'public/images/movie_posters/friday.jpg',
     title: 'Friday',
     description:
       "It's Friday, and Craig and Smokey must come up with $200 they owe a local bully or there won't be a Saturday."
   },
   {
-    path: 'video_library/Bad.Santa.2003.720p.BrRip.x264.YIFY.mp4',
+    video_path: 'video_library/Bad.Santa.2003.720p.BrRip.x264.YIFY.mp4',
+    poster_path: 'public/images/movie_posters/bad_santa.jpg',
     title: 'Bad Santa',
     description:
-      "It's Friday, and Craig and Smokey must come up with $200 they owe a local bully or there won't be a Saturday."
+      'A miserable conman and his partner pose as Santa and his Little Helper to rob department stores on Christmas Eve. But they run into problems when the conman befriends a troubled kid.'
   },
   {
-    path: 'video_library/Elf.2003.720p.BrRip.x264.YIFY.mp4',
+    video_path: 'video_library/Elf.2003.720p.BrRip.x264.YIFY.mp4',
     title: 'Elf',
+    poster_path: 'public/images/movie_posters/elf.jpg',
     description:
       'Buddy, a human, is raised amongst elves at the North Pole. When he discovers that he is not an elf, he travels to New York to search for his biological father.t'
   },
   {
-    path: 'video_library/Galaxy.Quest.1999.720p.BrRip.x264.YIFY.mp4',
+    video_path: 'video_library/Galaxy.Quest.1999.720p.BrRip.x264.YIFY.mp4',
+    poster_path: 'public/images/movie_posters/galaxy_quest.jpg',
     title: 'Galaxy Quest',
     description:
       'The alumni cast of a space opera television series have to play their roles as the real thing when an alien race needs their help. However, they also have to defend both Earth and the alien race from a reptilian warlord.'
   },
   {
-    path: 'video_library/Scrooged.1988.720p.BluRay.x264.YIFY.mp4',
+    video_path: 'video_library/Scrooged.1988.720p.BluRay.x264.YIFY.mp4',
+    poster_path: 'public/images/movie_posters/scrooged.jpg',
     title: 'Scrooged',
     description:
       'A selfish, cynical television executive is haunted by three spirits bearing lessons on Christmas Eve.'
   },
   {
-    path: 'video_library/Source.Code.2011.720p.BrRip.x264.YIFY.mp4',
+    video_path: 'video_library/Source.Code.2011.720p.BrRip.x264.YIFY.mp4',
+    poster_path: 'public/images/movie_posters/source_code.jpg',
     title: 'Source Code',
     description:
       "A soldier wakes up in someone else's body and discovers he's part of an experimental government program to find the bomber of a commuter train within 8 minutes."
