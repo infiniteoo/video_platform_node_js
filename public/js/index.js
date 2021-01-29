f7df29f1c25edd90ecc7cf48f273716ce5b135e9
Troy Dorman <troydorman@gmail.com>
2021-01-29 13:03:51 -0700

    got basic click to play functionality working from the "up next" window

--- public/js/index.js
+++ public/js/index.js
@@ -1,20 +1,62 @@
-document.addEventListener('DOMContentLoaded', init, false)
+document.addEventListener("DOMContentLoaded", init, false);
 
-/**
- * You can manipulate the video here
- * For example: Uncomment the code below and in the index to get a Start/Stop button
- */
-function init () {
-  /* const VP = document.getElementById('videoPlayer')
+function init() {
+  // if an item in the 'up next' list is clicked ->
+
+  $(".up-next-item").click(function () {
+    // eslint-disable-next-line no-undef
+    const titleVal = $(this).find("#title");
+    const targetMovie = titleVal[0].outerText;
+
+    // search the movies database for this target movie so we know the path to the video
+
+    $.get("/api/upnext", (data) => {
+      data.forEach((element) => {
+        if (element.title === targetMovie) {
+          var video = document.getElementById("videoPlayer");
+          var source = document.createElement("source");
+
+          source.setAttribute(
+            "src",
+            "video_library/" + element.video_path
+          );
+
+          video.appendChild(source);
+          video.play();
+
+          setTimeout(function () {
+            video.pause();
+
+            source.setAttribute(
+              "src",
+              element.video_path
+            );
+
+            video.load();
+            video.play();
+          }, 3000);
+
+       /*    $("#videoPlayer").html(
+            `<source src="/${element.video_path}" type="video/mp4">`
+          ); */
+
+          /* $.ajax({
+            url: finalURL,  
+            type: 'POST',
+            data: element.video_path
+          }).then(function(){
+            
+          }) */
+        }
+      });
+    });
+  });
+}
+
+/* const VP = document.getElementById('videoPlayer')
   const VPToggle = document.getElementById('toggleButton')
 
   VPToggle.addEventListener('click', function () {
     if (VP.paused) VP.play()
     else VP.pause()
   }) */
-
-  // eslint-disable-next-line no-undef
-  $.get('/api/upnext', (data) => {
-    console.log(data)
-  })
-}
--- routes/index.js
+++ routes/index.js
@@ -1,55 +1,61 @@
-const express = require('express')
-const router = express.Router()
-const fs = require('fs')
+const e = require("express");
+const express = require("express");
+const router = express.Router();
+const fs = require("fs");
 
-const data = require('../database')
+const data = require("../database");
 
 /* GET home page. */
-router.get('/', function (req, res, next) {
-  res.render('index', { movies: data })
-})
+router.get("/", function (req, res, next) {
+  res.render("index", { movies: data });
+});
 
-router.get('/api/upnext', function (req, res) {
-  res.json(data)
-})
+router.get("/api/upnext", function (req, res) {
+  res.json(data);
+});
 
-router.get('/video', function (req, res) {
-  const path = 'assets/sample.mp4'
-  const stat = fs.statSync(path)
-  const fileSize = stat.size
-  const range = req.headers.range
+router.get("/api/play", function (req, res) {});
+
+router.get("/video/:path", function (req, res) {
+  console.log("params path" + req.params.video_path)
+  const path = req.params.video_path
+  //const path = 'assets/sample.mp4'
+
+  const stat = fs.statSync(path);
+  const fileSize = stat.size;
+  const range = req.headers.range;
 
   if (range) {
-    const parts = range.replace(/bytes=/, '').split('-')
-    const start = parseInt(parts[0], 10)
-    const end = parts[1] ? parseInt(parts[1], 10) : fileSize - 1
+    const parts = range.replace(/bytes=/, "").split("-");
+    const start = parseInt(parts[0], 10);
+    const end = parts[1] ? parseInt(parts[1], 10) : fileSize - 1;
 
     if (start >= fileSize) {
       res
         .status(416)
-        .send('Requested range not satisfiable\n' + start + ' >= ' + fileSize)
-      return
+        .send("Requested range not satisfiable\n" + start + " >= " + fileSize);
+      return;
     }
 
-    const chunksize = end - start + 1
-    const file = fs.createReadStream(path, { start, end })
+    const chunksize = end - start + 1;
+    const file = fs.createReadStream(path, { start, end });
     const head = {
-      'Content-Range': `bytes ${start}-${end}/${fileSize}`,
-      'Accept-Ranges': 'bytes',
-      'Content-Length': chunksize,
-      'Content-Type': 'video/mp4'
-    }
+      "Content-Range": `bytes ${start}-${end}/${fileSize}`,
+      "Accept-Ranges": "bytes",
+      "Content-Length": chunksize,
+      "Content-Type": "video/mp4",
+    };
 
-    res.writeHead(206, head)
-    file.pipe(res)
+    res.writeHead(206, head);
+    file.pipe(res);
   } else {
     const head = {
-      'Content-Length': fileSize,
-      'Content-Type': 'video/mp4'
-    }
-    res.writeHead(200, head)
-    fs.createReadStream(path).pipe(res)
+      "Content-Length": fileSize,
+      "Content-Type": "video/mp4",
+    };
+    res.writeHead(200, head);
+    fs.createReadStream(path).pipe(res);
   }
-})
+});
 
-module.exports = router
+module.exports = router;
